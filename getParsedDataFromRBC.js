10b90560b85af622565c6e4a54d91999678960dd
Andrey <andrejbrylyakov@yandex.ru>
2022-10-30 14:19:12 +0300

    added rbc news

---
+++ getParsedDataFromRBC.js
@@ -0,0 +1,88 @@
+const puppeteer = require('puppeteer');
+const subDays = require('date-fns/subDays');
+const format = require('date-fns/format');
+const _ = require('lodash');
+const parse = require('date-fns/parse');
+const locale = require('date-fns/locale');
+const { delay } = require('./helpers/delay.js');
+const isMatch = require('date-fns/isMatch');
+
+const DATE_MASK = 'dd/MM/yyyy';
+
+const getPageData = async (page, selectorName) => {
+    return page.evaluate(({ selectorName }) => {
+        const elements = Array.from(document.querySelectorAll(selectorName));
+
+        return elements.map(element => {
+            const name = element.querySelector('.item__title').textContent;
+            const href = element.querySelector('.item__link').href;
+            const date = element.querySelector('.item__category').textContent;
+            // const dateRaw = element.querySelector('.news-preview__title').textContent;
+            // const date = dateRaw.split('.').join('/');
+
+           return {
+
+                href,
+                name,
+                date,
+           };
+        });
+    }, { selectorName });
+}
+
+const getPreparedDate = date => {
+    // const parsedDate = parse(date, 'dd.MM.yyyy', new Date(), { locale: locale.ru });
+    const localDateMask = 'd MMM, HH:mm'
+    let parsedDate = new Date();
+    
+    if (isMatch(date, localDateMask, { locale: locale.ru })) {
+        parsedDate = parse(date, localDateMask, new Date(), { locale: locale.ru });
+    }
+
+    const formattedDate = format(parsedDate, DATE_MASK);
+    return formattedDate;
+};
+
+async function getParsedDataFromRbcPage(pageName) {
+    const siteHref = `https://www.rbc.ru/${pageName}/?utm_source=topline`;
+    const previousDay = subDays(new Date(), 1);
+    const formattedPreviousDay = format(previousDay, DATE_MASK);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(siteHref);//, {waitUntil: 'load', timeout: 0}
+
+    const selectorName = '.js-load-container .item';
+    await page.waitForSelector(selectorName);
+
+    await delay(3000);
+
+    const listOfNewsRaw = await getPageData(page, selectorName);
+
+    const listOfNews = listOfNewsRaw.map(news => ({
+        ...news, 
+        name: _.trim(news.name).replace('\n', ' '),
+        date: getPreparedDate(news.date),
+    }));
+
+    await browser.close();
+
+    console.log(`РБК ${pageName}`, listOfNews.length);
+
+    return listOfNews.filter(news => news.date === formattedPreviousDay);
+}
+
+
+async function getParsedDataFromRBC() {
+    const economics = await getParsedDataFromRbcPage('economics');
+    const business = await getParsedDataFromRbcPage('business');
+    const finances = await getParsedDataFromRbcPage('finances');
+
+    return {
+        siteName: "РБК.ру",
+        siteHref: "https://www.rbc.ru/",
+        listOfNews: [...economics, ...business, ...finances],
+    };
+};
+
+module.exports = { getParsedDataFromRBC };
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const { getParsedDataFromRankings } = require('./getParsedDataFromRankings.js');
 const { getParsedDataFromAlfa } = require('./getParsedDataFromAlfa.js');
 const { getParsedDataFromVtb } = require('./getParsedDataFromVtb.js');
 const { getParsedDataFromRia }  = require('./getParsedDataFromRia.js');
+const { getParsedDataFromRBC }  = require('./getParsedDataFromRBC.js');
 
 const { getFilteredResult } = require('./getFilteredResult.js');
 const { convertAndSaveResult } = require('./convertAndSaveResult.js');
@@ -17,17 +18,16 @@ const { convertAndSaveResult } = require('./convertAndSaveResult.js');
     const parseResultAlfa = await getParsedDataFromAlfa();
     const parseResultVtb = await getParsedDataFromVtb();
     const parseResultRia = await getParsedDataFromRia();
+    const parseResultRBC = await getParsedDataFromRBC();
 
     const arrayOfResults = [parseResultLentaRu, parseResultBankiRu, parseResultPSB,
-        parseResultRankings, parseResultAlfa, parseResultVtb, parseResultRia];
+        parseResultRankings, parseResultAlfa, parseResultVtb, parseResultRia, parseResultRBC];
     const filteredArrayOfResults = getFilteredResult(arrayOfResults);
 
     const flatNewsArray = filteredArrayOfResults.reduce((acc, site) => ([...acc, ...site.listOfNews]), []);
     convertAndSaveResult(flatNewsArray);
 })();
 
-
-// https://ria.ru/economy/
 // https://www.rbc.ru/business/?utm_source=topline
 // https://www.raexpert.ru/
 // www.sberbank.ru не открывается без впн
